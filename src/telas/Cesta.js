14b1dd8c0806161a120b08359704d28281f527b1
Guilherme Ferreira <guilhermeferreira0711@gmail.com>
2023-05-05 13:52:11 -0300

    Tela Cesta de verduras

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Image, StyleSheet , Dimensions, Text} from 'react-native';
-
+import { Image, StyleSheet , View, Dimensions, Text} from 'react-native';
 import topo from '../../assets/topo.png';
 
 const width = Dimensions.get('screen').width
@@ -8,18 +7,44 @@ const width = Dimensions.get('screen').width
 export default function Cesta(){
   return<> 
     <Image source={topo} style={estilos.topo}/>
-    <Text>Detalhes da cesta</Text>
-  </>
-    
+    <Text style={estilos.titulo}>Detalhe da cesta</Text>
+    <View  style={estilos.cesta}>
+      <Text style={estilos.nome}>Cesta de verduras</Text>
+      <Text>Jenny Jack Farm</Text>
+      <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda para sua sozinha</Text>
+      <Text>R$40,00</Text>
+    </View>
+  </>  
 }
-
 const estilos = StyleSheet.create ({
-    topo: {
-      width: "100%",
-      height: 578 / 768 * width,
+  topo: {
+    width: "100%",
+    height: 578 / 768 * width,
+  },
 
-    }
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 26,
+    color: "white",
+    fontWeight: "bold",
+    padding: 16,
+  },
+
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
   
-  
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontWeight: "bold"
+
+
+  }
 })
   
